e6a68c4df903ae3bf4c7a9e70a20b7b16eec5b54
Francis Mangala <inestotofurume@FrancisM.local>
2021-04-17 02:04:40 -0400

    employee data complte

--- Employee_Challenge_starter_code.sql
+++ Employee_Challenge_starter_code.sql
@@ -1,3 +1,4 @@
+DROP TABLE retirement_challenge_info CASCADE;
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (______) _____,
 ______,
@@ -7,3 +8,37 @@ ______
 INTO nameyourtable
 FROM _______
 ORDER BY _____, _____ DESC;
+--1) Retrieve data from employees table
+SELECT emp_no
+	,first_name
+	,last_name
+FROM employees_2
+--2) Retrieve data from titles table
+SELECT title
+	,from_date
+	,to_date
+FROM titles_2
+
+--3) Create a new table using INTO clause
+SELECT emp_no
+	,first_name
+	,last_name
+INTO retirement_info_challenge
+FROM employees_2
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+Select * from retirement_info_challenge
+
+--4-to-7) Join both tables on the primay key. Using the INNER JOIN for these tables
+SELECT rci.emp_no,
+	rci.first_name,
+	rci.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM retirement_info_challenge as rci
+LEFT JOIN titles_2 as ti
+ON rci.emp_no = ti.emp_no
+ORDER BY rci.emp_no;
+
+SELECT * FROM retirement_titles;
---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,73 @@
+--1) Retrieve data from employees table
+SELECT emp_no
+	,first_name
+	,last_name
+FROM employees_2
+--2) Retrieve data from titles table
+SELECT title
+	,from_date
+	,to_date
+FROM titles_2
+
+--3) Create a new table using INTO clause
+SELECT emp_no
+	,first_name
+	,last_name
+INTO retirement_info_challenge
+FROM employees_2
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+Select * from retirement_info_challenge
+
+--4-to-7) Join both tables on the primay key. Using the INNER JOIN for these tables
+SELECT rci.emp_no,
+	rci.first_name,
+	rci.last_name,
+	ti.title,
+	ti.from_date,
+	ti.to_date
+INTO retirement_titles
+FROM retirement_info_challenge as rci
+LEFT JOIN titles_2 as ti
+ON rci.emp_no = ti.emp_no
+ORDER BY rci.emp_no;
+
+SELECT * FROM retirement_titles;
+
+--9
+SELECT first_name
+	,last_name
+	,title
+FROM retirement_titles
+
+--10 - 14-- Use DISTINCT ON statement
+SELECT DISTINCT ON (emp_no) emp_no
+	,first_name
+	,last_name
+	,title
+INTO unique_titles
+FROM retirement_titles
+ORDER BY emp_no,
+  	to_date DESC
+	
+SELECT * FROM unique_titles;
+
+--15 Employees about to retire
+SELECT title
+INTO retiring_titles
+FROM unique_titles
+
+SELECT DISTINCT COUNT (title),title FROM retiring_titles
+GROUP BY title
+ORDER BY count DESC
+
+--DELIVERABLE 2
+
+SELECT emp_no
+	,first_name
+	,last_name
+	,birth_date
+FROM employees_2;
+
+SELECT from_date,to_date FROM dept_manager_2_1;
+
+SELECT title from titles_2;
